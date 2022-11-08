ab891cfdc2c4d5ac131800e1bcb0c6d24c46587b
darwin1501 <darwin.pablo1501@gmail.com>
2022-11-08 10:43:18 +0800

    add landing page design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import './UtilStyle.css'
+import Block from './components/Block'
 
-function App() {
+function App () {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main>
+        <div className='title-container'>
+          <h1 className='m-0'>Memory Blocks</h1>
+          <p className='m-0'>How much pattern you can memorize?</p>
+        </div>
+        <div className='flex flex-center'>
+          <div className='blocks-container'>
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+            <Block value={1} />
+          </div>
+        </div>
+        <div className='flex flex-center'>
+          <div className='btn-play-container'>
+            <button className='btn-play' onClick={play}>
+              Play
+            </button>
+          </div>
+        </div>
+        <footer>
+          <p>Created by: Darwin</p>
+        </footer>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Block.js
@@ -0,0 +1,9 @@
+import React from "react";
+import BlockStyle from './BlockStyle.module.css'
+
+export default function Block(props) {
+    return (
+        <div className={BlockStyle.block}>
+        </div>
+    )
+}
